e189bf9f97c2f8c40b32bf1dd5ccc6455dce37af
sofymiranda <mirandaspfya@gmail.com>
2023-10-25 11:33:03 -0300

    atividade 22

---
+++ atv22.py
@@ -0,0 +1,5 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+list={1 , 50}
+for list in range (1,50):
+   if list % 2 == 0:
+      print (list)
